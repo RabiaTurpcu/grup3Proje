fe891f3ce77038dc0ea3cafd23d5d1956a2f497a
Jasi <mzng3243@gmail.com>
2023-03-30 23:28:11 +0200

    aa

--- src/test/java/utilities/Mustafa.java
+++ src/test/java/utilities/Mustafa.java
@@ -3,5 +3,8 @@ package utilities;
 public class Mustafa {
     public static void main(String[] args) {
         System.out.println("Mustafa");
+        System.out.println("aa");
+        System.out.println("bb");
+
     }
 }
